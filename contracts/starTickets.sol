43687f8d2f67d14af72e1497448fe14a9d1649c8
Jomari Peterson <jomari.peterson@gmail.com>
2023-10-16 22:25:45 -0700

    Integrate comments from Primata around funding and simplifying the mapping

--- contracts/starTickets.sol
+++ contracts/starTickets.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 interface IStarsArena {
     function buyShares(address sharesSubject, uint256 amount) external payable;
     function sellShares(address sharesSubject, uint256 amount) external payable;
-    function getBuyPrice(address sharesSubject, uint256 amount) external view returns (uint256);
-    function getSellPrice(address sharesSubject, uint256 amount) external view returns (uint256);
     function getBuyPriceAfterFee(address sharesSubject, uint256 amount) external view returns (uint256);
     function getSellPriceAfterFee(address sharesSubject, uint256 amount) external view returns (uint256);
 }
@@ -15,7 +13,7 @@ interface IStarsArena {
 contract StarTickets {
     IStarsArena public starsArena;
 
-    mapping(address => address) public subjectToToken;
+    mapping(address => IERC20Burnable) public subjectToToken;  // already correctly defined
 
     constructor(address _starsArena) {
         starsArena = IStarsArena(_starsArena);
@@ -24,29 +22,36 @@ contract StarTickets {
     function buyTicket(address subject, uint256 amount) public payable {
         require(msg.value >= amount, "Insufficient funds sent");
 
-        uint256 buyPrice = starsArena.getBuyPrice(subject, amount);
-        require(msg.value >= buyPrice, "Insufficient funds for ticket price");
+        uint256 buyPrice = starsArena.getBuyPriceAfterFee(subject, amount);
+        require(msg.value >= buyPrice, "Insufficient funds for ticket price including fee");
 
+        // Buy the ticket
         starsArena.buyShares{value: buyPrice}(subject, amount);
 
-        address tokenAddress = subjectToToken[subject];
-        if (tokenAddress == address(0)) {
-            // Deploy a new ERC20 for this subject
-            ERC20Burnable newToken = new ERC20Burnable("Ticket Token", "TKT");
-            subjectToToken[subject] = address(newToken);
-            tokenAddress = address(newToken);
+        // Return remaining funds
+        uint256 remainingFunds = msg.value - buyPrice;
+        if (remainingFunds > 0) {
+            payable(msg.sender).transfer(remainingFunds);
         }
 
-        IERC20(tokenAddress).mint(msg.sender, amount);
+        // Now using IERC20Burnable interface
+        IERC20Burnable token = subjectToToken[subject];
+        if (address(token) == address(0)) {
+            // Deploy a new ERC20 for this subject
+            ERC20Burnable newToken = new ERC20Burnable("Ticket Token", "TKT");
+            subjectToToken[subject] = newToken;
+            token = newToken;
+        }
+        token.mint(msg.sender, amount);  // directly use the mapped interface
     }
 
     function sellTicket(address subject, uint256 amount) public {
-        require(subjectToToken[subject] != address(0), "Token for subject not found");
+        IERC20Burnable token = subjectToToken[subject];
+        require(address(token) != address(0), "Token for subject not found");
 
-        address tokenAddress = subjectToToken[subject];
-        IERC20(tokenAddress).burnFrom(msg.sender, amount);
+        token.burnFrom(msg.sender, amount);  // directly use the mapped interface
 
-        uint256 sellPrice = starsArena.getSellPrice(subject, amount);
+        uint256 sellPrice = starsArena.getSellPriceAfterFee(subject, amount);
         starsArena.sellShares(subject, amount);
 
         payable(msg.sender).transfer(sellPrice);
