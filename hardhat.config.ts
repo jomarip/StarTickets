4426cc237fc306a5464d1a507df58de2c5053c8c
Jomari Peterson <jomari.peterson@gmail.com>
2023-10-21 10:25:32 -0700

    Trying alternative config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,39 +14,10 @@ import { ethers } from "ethers"
 
 dotenv.config()
 
-if (process.env.HARDHAT_FORK) {
-  process.env["HARDHAT_DEPLOY_FORK"] = process.env.HARDHAT_FORK
-}
 
 
 let config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
-  namedAccounts: {
-      deployer: {
-          default: 0, // account index 0 will be the deployer by default
-          hardhat: 0, // for the hardhat network, account index 0 will be the deployer
-          fuji: 0,    // for the fuji network, account index 0 will be the deployer
-          avalanche: 0, // for the mainnet, account index 0 will be the deployer
-      },
-  },
-  networks: {
-    hardhat: {
-      forking: {
-        url: "https://api.avax.network/ext/bc/C/rpc",
-        accounts: [process.env.PRIVATE_KEY],
-      },
-      deploy: ["./deploy"],
-    },
-    fuji: {
-      url: "https://api.avax-test.network/ext/bc/C/rpc",
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    avalanche: {
-      url: "https://api.avax.network/ext/bc/C/rpc",
-      deploy: ["./deploy/mainnet/"],
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
   paths: {
     sources: "./contracts",
     artifacts: "./build/artifacts",
@@ -76,71 +47,8 @@ let config: HardhatUserConfig = {
   mocha: {
     timeout: 200000,
   },
-  namedAccounts: {
-    deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-      42161: 0, // use the same address on arbitrum mainnet
-      10: 0, // use the same address on optimism mainnet
-      250: 0, // use the same address on fantom mainnet
-    },
-    libraryDeployer: {
-      default: 1, // use a different account for deploying libraries on the hardhat network
-      1: 0, // use the same address as the main deployer on mainnet
-      42161: 0, // use the same address on arbitrum mainnet
-      10: 0, // use the same address on optimism mainnet
-      250: 0, // use the same address on fantom mainnet
-    },
-  },
  
 }
 
-if (process.env.ETHERSCAN_API) {
-  config = { ...config, etherscan: { apiKey: process.env.ETHERSCAN_API } }
-}
-
-if (process.env.ACCOUNT_PRIVATE_KEYS) {
-  config.networks = {
-    ...config.networks,
-    avalanche: {
-      ...config.networks?.mainnet,
-      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
-    },
-    arbitrum_mainnet: {
-      ...config.networks?.arbitrum_mainnet,
-      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
-    },
-    optimism_mainnet: {
-      ...config.networks?.optimism_mainnet,
-      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
-    },
-    fantom_mainnet: {
-      ...config.networks?.fantom_mainnet,
-      accounts: JSON.parse(process.env.ACCOUNT_PRIVATE_KEYS),
-    },
-  }
-}
-
-if (process.env.FORK_MAINNET === "true" && config.networks) {
-  console.log("FORK_MAINNET is set to true")
-  config = {
-    ...config,
-    networks: {
-      ...config.networks,
-      hardhat: {
-        ...config.networks.hardhat,
-        forking: {
-          url: process.env.ALCHEMY_API ? process.env.ALCHEMY_API : "",
-        },
-        chainId: 1,
-      },
-    },
-    external: {
-      deployments: {
-        localhost: ["deployments/mainnet"],
-      },
-    },
-  }
-}
 
 export default config
